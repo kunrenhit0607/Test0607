2c3240ce42896c08896fd4d369bd067d3220f252
kunren16 <kunren16@macbook15.local>
2016-06-07 10:09:02 +0900

    New Create

---
+++ Test0607/Testclass.swift
@@ -0,0 +1,13 @@
+//
+//  Testclass.swift
+//  Test0607
+//
+//  Created by kunren16 on 2016/06/07.
+//  Copyright © 2016年 A.Hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class Testclass: NSObject {
+
+}
